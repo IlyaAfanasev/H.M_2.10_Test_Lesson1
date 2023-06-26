42841d56f9053d62d77d66aff8021c4467213601
IlyuAfanasev <gecktor.86@mail.ru>
2023-06-26 21:59:46 +0400

    Initial commit

---
+++ src/test/java/pro/sky/java/course2/hm_2_10_testlesson1/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.hm_2_10_testlesson1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
